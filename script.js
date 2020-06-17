2aeb1d995ade0e646cb5bf478b46bc3f4d3e310b
Roberto Riccioli <riccioli.rob@gmail.com>
2020-06-17 18:00:11 +0200

    Form e ticket

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Definisco tutte le variabili:
 
-//Nome e cognome
+//Nome. cognome ed età
 
 var name = document.getElementById('name-form').value;
 var familyName = document.getElementById('family-name-form').value;
+var categories = document.getElementById('categories').value;
 
-console.log(name)
-console.log(familyName)
+console.log(name);
+console.log(familyName);
+console.log(categories);
 
 // Km da percorrere
 
-var km = document.getElementById('km').value;
+var km = document.getElementById('km-form').value;
 var price = km * 0.21;
+
+console.log(km);
+
+//bottoni on e off
+
+var ticketOn = document.getElementById('ticket-on');
+var ticketOff = document.getElementById('ticket-off');
 
 
 //Condizioni da porre in basse all'età
@@ -25,5 +34,20 @@ if (categories == "kid") {
     var finalPrice = price - discount;
 
 } else {
-    price = price;
+    finalPrice = price;
 }
+
+
+ticketOn.addEventListener("click" , function(){
+
+  document.getElementByID('full-name').innerHTML = "Messere: " + name + " " + categories + " della casata: " + familyName;
+  document.getElementById('full-price').innerHTML = "La tariffa per lo vostro percorso in carrozza ammonta a " + finalPrice.toFixed(2) + " Fiorini";
+
+})
+
+ticketOff.addEventListener("click" , function(){
+
+  document.getElementByID('full-name').innerHTML = "Messere: " + name + " " + categories + " della casata: " + familyName;
+  document.getElementById('full-price').innerHTML = "La tariffa per lo vostro percorso in carrozza ammonta a " + finalPrice.toFixed(2) + " Fiorini";
+
+})
